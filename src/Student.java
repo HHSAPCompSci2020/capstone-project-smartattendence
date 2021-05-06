b203dfc2f9dbecbcff8b97dee25806c268d207ae
zhao-xinyu <69945778+zhao-xinyu@users.noreply.github.com>
2021-05-06 11:00:37 -0700

    added methods

--- src/Student.java
+++ src/Student.java
@@ -1,11 +1,39 @@
 /**
  * This class represent a Student.
+ * 
  * @author Xinyu Zhao
  *
  */
 public class Student {
-	
-	int STUDENT_ID;
-	String STUDENT_NAME;
+
+	private final int STUDENT_ID;
+	private final String STUDENT_NAME;
+	private boolean attendance = false;
+
+	public Student() {
+		STUDENT_ID = 0;
+		STUDENT_NAME = "";
+	}
+
+	public Student(int id, String name) {
+		STUDENT_ID = id;
+		STUDENT_NAME = name;
+	}
+
+	public int getID() {
+		return STUDENT_ID;
+	}
+
+	public String getName() {
+		return STUDENT_NAME;
+	}
+
+	public boolean getAttendance(){
+		return attendance;
+	}
+
+	public void addPhoto(){
+		//adds photo
+	}
 
 }
