d92d252e4ee29aa35e042b93772d46964e49cb76
zhao-xinyu <69945778+zhao-xinyu@users.noreply.github.com>
2021-05-06 10:48:25 -0700

    added methods

--- src/School.java
+++ src/School.java
@@ -1,15 +1,25 @@
 import java.util.ArrayList;
 
 /**
- * @author Xinyu Zhao
- * This is the School class, where it will contain Classroom
+ * @author Xinyu Zhao This is the School class, where it will contain Classroom
  */
 public class School {
-	
+
 	ArrayList<Classroom> s;
-	
+
 	public School() {
-		
+
+	}
+
+	// adds a Classroom
+	public void add(Classroom classroom) {
+		s.add(classroom);
+
+	}
+
+	// removes a Classroom
+	public void remove(Classroom classroom) {
+		s.remove(classroom);
 	}
 
 }
