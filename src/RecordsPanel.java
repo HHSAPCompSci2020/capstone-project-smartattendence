a5bf8eaf2cd6ecc05b0612af67dcd8a6414aa3f6
zhao-xinyu <69945778+zhao-xinyu@users.noreply.github.com>
2021-05-12 13:18:17 -0700

    added recordsPanel

---
+++ src/RecordsPanel.java
@@ -0,0 +1,81 @@
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+ 
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+ 
+public class RecordsPanel extends JPanel {
+   private SQLiteManager sqlManager;
+   private String dataDir;
+ 
+   List<Classroom> allClassrooms;
+   DefaultListModel<String> classroomListModel;
+   JList<String> classroomList;
+ 
+   GridBagConstraints constraints = new GridBagConstraints();
+ 
+   static JButton showButton = new JButton("Show Attendence");
+ 
+   public RecordsPanel(String dataDir, SQLiteManager sqlManager) {
+       this.dataDir = dataDir;
+       this.sqlManager = sqlManager;
+      
+       setLayout(new GridBagLayout());
+      
+       allClassrooms = sqlManager.getAllClassrooms();
+ 
+       JPanel cmdPanel = new JPanel();
+       cmdPanel.setLayout(new GridLayout(3, 1));
+       cmdPanel.add(showButton);
+ 
+       classroomListModel = new DefaultListModel<String>();
+       if (allClassrooms != null) {
+           for(Classroom classroom : allClassrooms) {
+               classroomListModel.addElement(classroom.getName());
+           }
+       }
+       classroomList = new JList<String>(classroomListModel);
+      
+       showButton.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+           }
+       });
+ 
+ 
+       classroomList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+       classroomList.setLayoutOrientation(JList.VERTICAL);
+       classroomList.setVisibleRowCount(-1);
+       JScrollPane listScroller = new JScrollPane(classroomList);
+       listScroller.setPreferredSize(new Dimension(200, 200));
+ 
+       GridBagConstraints gbc = new GridBagConstraints();
+       gbc.fill = GridBagConstraints.NONE;
+       gbc.weightx = 0;
+       gbc.gridx = 0;
+       gbc.gridy = 0;
+ 
+       add(cmdPanel, gbc);
+ 
+       gbc.fill = GridBagConstraints.VERTICAL;
+       gbc.weighty = 1.0;
+       gbc.weightx = 1;
+       gbc.gridx = 1;
+       gbc.gridy = 0;
+       add(listScroller, gbc);
+   }
+}
