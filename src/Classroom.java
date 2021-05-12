208d1648dcfee76f98487771ad858b152f44bff3
zhao-xinyu <69945778+zhao-xinyu@users.noreply.github.com>
2021-05-12 13:16:57 -0700

    classroom

--- src/Classroom.java
+++ src/Classroom.java
@@ -9,40 +9,70 @@ import java.util.ArrayList;
 
 public class Classroom {
 
-	ArrayList<StudentPanel> classroom;
-
-	/**
-	 * @pre cannot have two students with the same name!!
-	 */
-	public Classroom() {
-		classroom = null;
+	ArrayList<Student> classroom;
+	   String name;
+	 
+	   /**
+	    * @pre cannot have two students with the same name!!
+	    */
+	   public Classroom() {
+	       classroom = null;
+	   }
+	 
+	   /**
+	    * @pre cannot have two students with the same name!!
+	    */
+	   public Classroom(String name) {
+	       this.name = name;
+	   }
+	 
+	   /**
+	    * This method adds a new student to the class
+	    *
+	    * @param s Takes in a Student object
+	    * @post ArrayList classroom has a new student
+	    */
+	   public void addStudent(Student s) {
+	       classroom.add(s);
+	   }
+	 
+	   /**
+	    * This method removes a student from the class
+	    *
+	    * @param s Takes in a Student object
+	    * @post ArrayList classroom has one less student
+	    */
+	   public void removeStudent(Student s) {
+	       classroom.remove(s);
+	 
+	   }
+	  
+	   public String getName() {
+	       return name;
+	   }
+	  
+	   public void setName(String name) {
+	       this.name = name;
+	   }
+	 
+	   public int getPeriod() {
+	       // TODO Auto-generated method stub
+	       return -1;
+	   }
+	 
+	   public String getTeacher() {
+	       // TODO Auto-generated method stub
+	       return null;
+	   }
+	 
+	   public void setPeriod(int period) {
+	       // TODO Auto-generated method stub
+	      
+	   }
+	 
+	   public void setTeacher(String teacher) {
+	       // TODO Auto-generated method stub
+	      
+	   }
 	}
-
-	/**
-	 * @pre cannot have two students with the same name!!
-	 */
-	public Classroom(ArrayList<StudentPanel> c) {
-		classroom = c;
-	}
-
-	/**
-	 * This method adds a new student to the class
-	 * 
-	 * @param s Takes in a Student object
-	 * @post ArrayList classroom has a new student
-	 */
-	public void addStudent(StudentPanel s) {
-		classroom.add(s);
-	}
-
-	/**
-	 * This method removes a student from the class
-	 * 
-	 * @param s Takes in a Student object
-	 * @post ArrayList classroom has one less student
-	 */
-	public void removeStudent(StudentPanel s) {
-		classroom.remove(s);
-
-	}
-}
+	 
